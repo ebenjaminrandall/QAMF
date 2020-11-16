fc81fd5be8f8db9ac8072eae34cba3df41f6f570
ebenjaminrandall <ebenjaminrandall@users.noreply.github.com>
2020-11-16 17:20:24 -0500

    Fixed Pi cotransporter eqn and param values

--- QAMFmodel_invivo/model.m
+++ QAMFmodel_invivo/model.m
@@ -248,21 +248,15 @@ J_ANT = E_ANT * num / den;
 % H2PO42-_x + H+_x = H2PO42-_c + H+_c
 
 % Constant
-k_PiC = 0.0016125;  % mol (L cell)^(-1)
+k_PiC = 1.61e-3;  % mol (L cell)^(-1)
 
 % [H2P04-] 
 HPi_c = Pi_c * (H_c / K_HPi);
 HPi_x = Pi_x * (H_x / K_HPi);
 
-% convert to mol (L cell)^(-1)
-H_c_cell = H_c * (V_c * W_c + V_m * W_i); 
-H_x_cell = H_x * V_m * W_x; 
-HPi_c_cell = HPi_c * (V_c * W_c + V_m * W_i); 
-HPi_x_cell = HPi_x * V_m * W_x; 
-
 % Flux (mol (s * L mito)^(-1))
-J_PiC = E_PiC * (H_c_cell * HPi_c_cell - H_x_cell * HPi_x_cell ) / ... 
-    (k_PiC * (1 + HPi_c_cell/k_PiC) * (1 + HPi_x_cell/k_PiC));
+J_PiC = E_PiC * (H_c * HPi_c - H_x * HPi_x ) / ... 
+    (k_PiC +  HPi_c);
 
 %% Creatine kinase reaction
 % ADP3- + CrP2- + H+ = ATP4- + Cr
